0833a66deff221e505ab1f8b3991134c70ec6a68
Júlia Bettiol de Oliveira <143723147+JuliaBettiol@users.noreply.github.com>
2024-08-29 12:03:36 -0300

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Júlia Bettiol de Oliveira on 29/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
